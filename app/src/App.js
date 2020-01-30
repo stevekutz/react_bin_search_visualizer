41657cf8853ecc78e4a5ab8e7026f9c6cf32c6ee
stevekutz <stkutz@gmail.com>
2020-01-29 19:26:39 -0600

    building basic interface

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'reinspect';
 // Big O Notation
 // you need to know how running time increases as the list size increasing
 
 
 function App() {
+   const [input, setInput] = useState('', 'input State')
+
+
   return (
     <div className="App">
-
+        <div>
+            <input 
+            type = 'text' 
+            placeholder = 'enter max list value'
+            onChange = {(e) => setInput(e.target.value)}
+            />
+            <button
+            
+            > Build sorted list</button>
+        </div>
     </div>
   );
 }
